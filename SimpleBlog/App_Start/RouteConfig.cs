cc0768f75c429ef52caae27688a89bae97eae103
emrah karaman <ekaraman89@hotmail.com>
2018-03-14 14:50:23 +0300

    route config links add

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -18,8 +18,10 @@ namespace SimpleBlog
 
             routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
 
-            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" });
-            routes.MapRoute("list", "list", new { controller = "Auth", action = "list" });
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("list", "list", new { controller = "Auth", action = "list" }, namespaces);
+            routes.MapRoute("PostList", "PostList", new { controller = "Posts", action = "List" }, namespaces);
+
         }
     }
 }
