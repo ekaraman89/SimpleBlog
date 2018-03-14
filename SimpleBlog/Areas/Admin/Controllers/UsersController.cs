449f3be3862b0b0711701b27a11edde36db128c9
emrah karaman <ekaraman89@hotmail.com>
2018-03-14 14:16:53 +0300

    admin users controller add

---
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return Content("Users Controller Index Action");
+        }
+
+        public ActionResult New()
+        {
+            return Content("Users Controller New Action");
+        }
+
+        public ActionResult Update()
+        {
+            return Content("Users Controller Update Action");
+        }
+
+        public ActionResult Delete()
+        {
+            return Content("Users Controller Delete Action");
+        }
+    }
+}
