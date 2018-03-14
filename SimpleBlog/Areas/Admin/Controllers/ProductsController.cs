b71feabaa40bbac5952adbe276e5252cedf634f9
emrah karaman <ekaraman89@hotmail.com>
2018-03-14 14:16:35 +0300

    admin product add

---
+++ SimpleBlog/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        public ActionResult Index()
+        {
+            return Content("Products Controller Index Action");
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            return Content("Products Controller Edit Action with parameter : " + id.ToString());
+        }
+    }
+}
