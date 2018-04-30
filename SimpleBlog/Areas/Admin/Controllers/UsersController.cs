45559482d4d393e1a8fb6a01c675ed872c1ff365
emrah karaman <ekaraman89@hotmail.com>
2018-04-30 18:55:43 +0300

    return view added

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,14 @@ using System.Web.Mvc;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "admin")]
+    [SelectedTab("Users")]
     public class UsersController : Controller
     {
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return Content("Users Controller Index Action");
+            return View();
         }
 
         public ActionResult New()
