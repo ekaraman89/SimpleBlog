38921f41fc45edd5df7b4ab577cea58047b23dab
emrah karaman <ekaraman89@hotmail.com>
2018-05-12 01:28:27 +0300

    database class configured

--- SimpleBlog/Database.cs
+++ SimpleBlog/Database.cs
@@ -1,4 +1,7 @@
-﻿using NHibernate.Cfg;
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using SimpleBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,40 @@ namespace SimpleBlog
 {
     public static class Database
     {
+        private const string SessionKey = "SimpleBlog.Database.SessionKey";
+
+        private static ISessionFactory _sessionFactory;
+
+        public static ISession Session
+        {
+            get { return (ISession)HttpContext.Current.Items[SessionKey]; }
+        }
+
         public static void Configure()
         {
             var config = new Configuration();
-            //config.Configure();
+            config.Configure();
+
+            var mapper = new ModelMapper();
+            mapper.AddMapping<UserMap>();
+
+            config.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+            _sessionFactory = config.BuildSessionFactory();
         }
 
-        public static void OpenSession() { }
+        public static void OpenSession()
+        {
+            HttpContext.Current.Items[SessionKey] = _sessionFactory.OpenSession();
+        }
 
-        public static void CloseSession() { }
+        public static void CloseSession()
+        {
+            var session = HttpContext.Current.Items[SessionKey] as ISession;
+            if (session != null)
+                session.Close();
+
+            HttpContext.Current.Items.Remove(SessionKey);
+        }
     }
 }
