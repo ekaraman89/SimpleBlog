9ae8b94f5a55c0fefa235c3c51eb75b5d01f0f00
emrah karaman <ekaraman89@hotmail.com>
2018-05-12 01:27:19 +0300

    user view model added

---
+++ SimpleBlog/Areas/Admin/ViewModels/User.cs
@@ -0,0 +1,10 @@
+﻿using SimpleBlog.Models;
+using System.Collections.Generic;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class UserIndex
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
